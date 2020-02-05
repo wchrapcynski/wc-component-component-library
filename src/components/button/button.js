625231ec676b3d2d4334c91388b833320bd2415b
William Chrapcynski <wchrapcynski@gmail.com>
2020-02-05 17:20:51 -0500

    Refactored type selection in button component

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,17 +3,8 @@ import './../../styles/common.scss'
 import './button.scss'
 
 function Button(props) {
-	let localClass = "";
-	let types = ["primary", "disabled"];
 
-	// Checks for type and sets localClass when necessary
-	for(let i = 0; i < types.length; i++) {
-		if(props.type === types[i]) {
-			localClass = props.type;
-		}
-	}
-
-	const cls = `bgYellow	fontBlack pad8 width100 radius15 ${localClass}`.trim();
+	const cls = `bgYellow	fontBlack pad8 width100 radius15 ${props.type}`.trim();
 
   return (
     <div className='button'>
