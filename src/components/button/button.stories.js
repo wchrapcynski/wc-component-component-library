fdf8fc94192d0931823e9b90421bc8036b2d8001
William Chrapcynski <wchrapcynski@gmail.com>
2020-02-06 12:09:01 -0500

    Added success button

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -5,4 +5,5 @@ import Button from './button'
 storiesOf("Button", module)
 	.add("Button - primary", () => <Button type="primary">primary</Button>)
 	.add("Button - secondary", () => <Button type="secondary">secondary</Button>)
+	.add("Button - success", () => <Button type="success">success</Button>)
 	.add("Button - disabled", () => <Button type="disabled">disabled</Button>)
