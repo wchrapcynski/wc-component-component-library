eaaa1e2d465129c06a003df4d4c5487286b8737d
William Chrapcynski <wchrapcynski@gmail.com>
2020-02-13 16:48:03 -0500

    Attempting to fix Netlify deployment

--- src/components/buttons/buttons.stories.js
+++ src/components/buttons/buttons.stories.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import Button from './Buttons.js'
+import Buttons from "./Buttons";
 
-storiesOf("Button", module)
-	.add("Button - primary", () => <Button type="primary" onClickHandle={action('Clicked!')}>Primary</Button>)
-	.add("Button - secondary", () => <Button type="secondary" onClickHandle={action('Clicked!')}>Secondary</Button>)
-	.add("Button - success", () => <Button type="success" onClickHandle={action('Clicked!')}>Success</Button>)
-	.add("Button - danger", () => <Button type="danger" onClickHandle={action('Clicked!')}>Danger</Button>)
-	.add("Button - warning", () => <Button type="warning" onClickHandle={action('Clicked!')}>Warning</Button>)
-	.add("Button - info", () => <Button type="info" onClickHandle={action('Clicked!')}>Info</Button>)
-	.add("Button - light", () => <Button type="light" onClickHandle={action('Clicked!')}>Light</Button>)
-	.add("Button - dark", () => <Button type="dark" onClickHandle={action('Clicked!')}>Dark</Button>)
-	.add("Button - disabled", () => <Button type="disabled" onClickHandle={action('Clicked!')}>Disabled</Button>)
+storiesOf("Buttons", module)
+  .add("Button - primary", () => (
+    <Buttons type='primary' onClickHandle={action("Clicked!")}>
+      Primary
+    </Buttons>
+  ))
+  .add("Button - secondary", () => (
+    <Buttons type='secondary' onClickHandle={action("Clicked!")}>
+      Secondary
+    </Buttons>
+  ))
+  .add("Button - success", () => (
+    <Buttons type='success' onClickHandle={action("Clicked!")}>
+      Success
+    </Buttons>
+  ))
+  .add("Button - danger", () => (
+    <Buttons type='danger' onClickHandle={action("Clicked!")}>
+      Danger
+    </Buttons>
+  ))
+  .add("Button - warning", () => (
+    <Buttons type='warning' onClickHandle={action("Clicked!")}>
+      Warning
+    </Buttons>
+  ))
+  .add("Button - info", () => (
+    <Buttons type='info' onClickHandle={action("Clicked!")}>
+      Info
+    </Buttons>
+  ))
+  .add("Button - light", () => (
+    <Buttons type='light' onClickHandle={action("Clicked!")}>
+      Light
+    </Buttons>
+  ))
+  .add("Button - dark", () => (
+    <Buttons type='dark' onClickHandle={action("Clicked!")}>
+      Dark
+    </Buttons>
+  ))
+  .add("Button - disabled", () => (
+    <Buttons type='disabled' onClickHandle={action("Clicked!")}>
+      Disabled
+    </Buttons>
+  ));
