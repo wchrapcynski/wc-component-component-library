2ab0234c034a0df5db04201e47ce77c2689ea006
William Chrapcynski <wchrapcynski@gmail.com>
2020-02-15 14:19:20 -0500

    Working on adding functionality to navbar tab items

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import "./App.css";
 import Header from "./components/header/header";
 
 function App() {
+  const navbarItems = ["Home", "About", "Contact"];
+
+  function navbarOnClickHandle(event) {
+    event.target.classList.remove("primary-tab")
+    event.target.classList.add("secondary-tab")
+  }
+
   return (
     <div className="App">
-      <Header></Header>
+      <Header navbarItems={navbarItems} onClickHandle={navbarOnClickHandle} />
     </div>
   );
 }
--- src/components/header/header.stories.js
+++ src/components/header/header.stories.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import Header from "./header";
 
 const navbarItems = ["Home", "About", "Contact"]
 
-storiesOf("Header", module).add("Header", () => <Header navbarItems={navbarItems} />);
+storiesOf("Header", module).add("Header", () => (
+  <Header navbarItems={navbarItems} onClickHandle={action("Clicked!")} />
+));
