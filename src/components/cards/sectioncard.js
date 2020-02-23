4755be49525e43072f2416d8981ea3f00ec8bab3
William Chrapcynski <wchrapcynski@gmail.com>
2020-02-23 18:37:58 -0500

    Added HR to section header

--- src/components/cards/sectioncard.js
+++ src/components/cards/sectioncard.js
@@ -9,9 +9,14 @@ function SectionCard(props) {
       id={props.id}>
       <div className={`${props.cardColorType} ${props.boxSize} radius15 card`}>
         <div className="card-header">
-          <div className="card-header-item font-size-2">{props.description}</div>
-          <div className="card-header-item">{props.toTop ? <a href="#Top">To Top</a> : ""}</div>
+          <div className="card-header-item font-size-2">
+            {props.description}
+          </div>
+          <div className="card-header-item">
+            {props.toTop ? <a href="#Top">To Top</a> : ""}
+          </div>
         </div>
+        <hr />
         <div className="card-body"></div>
         <div className="card-footer"></div>
       </div>
