abf39b041c4c16f85254fa85ad261acffc4967a4
William Chrapcynski <wchrapcynski@gmail.com>
2020-03-11 20:40:37 -0400

    Added text to find in on hover for icons

--- src/components/icons/socialmedia.js
+++ src/components/icons/socialmedia.js
@@ -3,13 +3,16 @@ import "./icons.scss";
 
 function Socialmedia(props) {
   return (
-    <div className="Socialmedia">
+    <div className="social-media">
       <a href={props.link} onClick={props.onClickHandle} target="_blank" rel="noopener noreferrer">
         <img
           src={require(`../../images/icons/${props.type}.svg`)}
           alt={props.type}
         />
       </a>
+      <div className="social-media-text">
+        {props.type.charAt(0).toUpperCase() + props.type.slice(1)}
+      </div>
     </div>
   );
 }
