0d92dfefef41b9abc2b0c6085aa266518ac3639f
William Chrapcynski <wchrapcynski@gmail.com>
2020-03-16 20:01:12 -0400

    Adjusted CSS behavior of social media icons

--- src/components/icons/socialmedia.js
+++ src/components/icons/socialmedia.js
@@ -4,12 +4,18 @@ import "./icons.scss";
 function Socialmedia(props) {
   return (
     <div className="social-media">
-      <a href={props.link} onClick={props.onClickHandle} target="_blank" rel="noopener noreferrer">
-        <img
-          src={require(`../../images/icons/${props.type}.svg`)}
-          alt={props.type}
-        />
-      </a>
+      <div className="social-media-img">
+        <a
+          href={props.link}
+          onClick={props.onClickHandle}
+          target="_blank"
+          rel="noopener noreferrer">
+          <img
+            src={require(`../../images/icons/${props.type}.svg`)}
+            alt={props.type}
+          />
+        </a>
+      </div>
       <div className="social-media-text">
         {props.type.charAt(0).toUpperCase() + props.type.slice(1)}
       </div>
@@ -17,4 +23,4 @@ function Socialmedia(props) {
   );
 }
 
-export default Socialmedia
+export default Socialmedia;
